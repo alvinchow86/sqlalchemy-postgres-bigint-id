bf7c4970c437157517b5dcf632971c4ec73a3d85
Alvin Chow <alvinchow86@gmail.com>
2020-05-14 15:44:58 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,15 @@ setup(
     name='sqlalchemy-postgres-bigid',
     version=__version__,
     description="SQLAlchemy Postgres Big Integer ID",
+    url='https://github.com/alvinchow86/sqlalchemy-postgres-bigid',
     author='Alvin Chow',
+    author_email='alvinchow86@gmail.com',
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
     packages=[
         'sqlalchemy_bigid',
     ],
@@ -18,4 +26,5 @@ setup(
         'sqlalchemy>=1.2',
         'python-dateutil>=2.7',
     ],
+    python_requires='>=3.6',
 )
