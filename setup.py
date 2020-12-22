39f9df454fda258fe0afe6b83e6a3e6f550ceee9
Alvin Chow <alvinchow86@gmail.com>
2020-12-22 14:30:42 -0800

    fix

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ __version__ = None
 exec(open('sqlalchemy_bigid/version.py').read())
 
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, 'README.md')) as f:
     long_description = f.read()
 
 setup(
